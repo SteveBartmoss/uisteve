8f79548ed354cfd73f84da116ce626d80bc835c2
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-16 14:00:00 -0600

    se agrego el componente de menu vertical

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { MainFrame } from './componentes/contenedores';
+import { MaenuVertical } from './componentes/MenuVertical';
 
 function App() {
   return (
     <MainFrame>
+      <MaenuVertical />
       <h1>Hola</h1>
     </MainFrame>
   );
