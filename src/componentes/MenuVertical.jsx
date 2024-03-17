cf2365687ab8e87683cda7456df786339b89c1e7
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-16 19:16:22 -0600

    se agregaron los elementos de la barra de navegacion

--- src/componentes/MenuVertical.jsx
+++ src/componentes/MenuVertical.jsx
@@ -1,8 +1,14 @@
- 
- export function MaenuVertical(){
-    <div className="menu-vertical">
-        <p>Dashboards</p>
-        <p>Ui Elements</p>
-        <p>Fomrs & Tables</p>
-    </div>
- }
+import { NavLink } from "react-router-dom";
+
+export function MenuVertical() {
+    return (
+        <div className="menu-vertical">
+            <div className="nav-bar">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/dash">Dashboards</NavLink>
+                <NavLink to="/dash">Ui Elements</NavLink>
+                <NavLink to="/dash">Fomrs & Tables</NavLink>
+            </div>
+        </div>
+    );
+}
