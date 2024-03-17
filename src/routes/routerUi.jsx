5e6e61dec2ce156510d93f9ce0dbc2674e8f43fd
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-17 12:57:03 -0600

    se agrego un enlace para los text fields

--- src/routes/routerUi.jsx
+++ src/routes/routerUi.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import { MenuVertical } from "../componentes/MenuVertical";
+import { TextField } from "../componentes/TextField";
 
 
 export function RuoterUi(){
@@ -9,6 +10,7 @@ export function RuoterUi(){
             <Routes>
                 <Route path="/" element={<h1>Home</h1>}  />
                 <Route path="/dash" element={<h1>Dash</h1>}  />
+                <Route path="/forms/texfield" element={<TextField />}  />
             </Routes>
         </BrowserRouter>
     );
