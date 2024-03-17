ec8537d0736595ef6003203f6aca5c0c08efb201
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-17 12:58:11 -0600

    se creo un componente para ls textfields

---
+++ src/componentes/TextField.jsx
@@ -0,0 +1,13 @@
+import "../App.css"
+
+export function TextField() {
+    return (
+        <>
+            <div className="entry-area">
+                <input className="basic" type="text" />
+                <div className="label-line">Ingresa tu nombre</div>
+            </div>
+
+        </>
+    );
+}
