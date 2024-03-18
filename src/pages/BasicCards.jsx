732db29c2676c710c553abed0f3abbf228a06240
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-17 18:07:53 -0600

    se implemento una pagina para mostrar las cards

--- src/pages/BasicCards.jsx
+++ src/pages/BasicCards.jsx
@@ -1,8 +1,17 @@
+import { Card } from "../componentes/Card";
 
 export function BasicCards(){
     return(
         <div>
             <h1>Basic Cards</h1>
+            <Card>
+                <h1>Esta es una card de prueba</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Facere pariatur molestiae amet, cupiditate inventore maiores reprehenderit, 
+                    ex earum neque ab libero eum quo fuga corporis tempora atque, praesentium ullam nisi.
+                </p>
+            </Card>
         </div>
     );
 }
