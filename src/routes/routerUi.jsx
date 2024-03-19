614fe98d8046627816ee2218ca33ba18972d1727
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-18 18:49:14 -0600

    se agrego una ruta para los paneles expandibles

--- src/routes/routerUi.jsx
+++ src/routes/routerUi.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import { MenuVertical } from "../componentes/MenuVertical";
 import { TextField } from "../componentes/TextField";
 import { BasicCards } from "../pages/BasicCards";
+import { Acordeones } from "../pages/ExpansionPanel";
 
 
 export function RuoterUi(){
@@ -13,6 +14,7 @@ export function RuoterUi(){
                 <Route path="/dash" element={<h1>Dash</h1>}  />
                 <Route path="/forms/texfield" element={<TextField />}  />
                 <Route path="/cards/basic" element={<BasicCards />}  />
+                <Route path="/acordeon" element={<Acordeones />} />
             </Routes>
         </BrowserRouter>
     );
