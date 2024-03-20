06d1c5ea1ffbfb348a2efe3161343ad1c35a26cb
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-19 21:48:07 -0600

    se agego el componente div row para que las cards tuvieran mejor acomodo

--- src/pages/BasicCards.jsx
+++ src/pages/BasicCards.jsx
@@ -1,30 +1,32 @@
 import { Card, CardAccions, CardLink, CardText, CardTitle } from "../componentes/Card";
+import { DivRow } from "../componentes/contenedores";
 
-export function BasicCards(){
-    return(
+export function BasicCards() {
+    return (
         <div>
             <h1>Basic Cards</h1>
-            <Card>
-                <CardTitle>Esta es una card de prueba</CardTitle>
-                <CardText>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Facere pariatur molestiae amet, cupiditate inventore maiores reprehenderit, 
-                    ex earum neque ab libero eum quo fuga corporis tempora atque, praesentium ullam nisi.
-                </CardText>
-            </Card>
-
-            <Card>
-                <CardTitle>Card con link</CardTitle>
-                <CardText>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                    Inventore commodi numquam nihil ad molestias similique voluptatem 
-                    blanditiis ex tempora, accusantium atque. Natus aliquam tempora 
-                    dolor cumque architecto dignissimos nesciunt debitis.
-                </CardText>
-                <CardAccions>
-                    <CardLink url={'https://stevechambitas.netlify.app/home'} text={'Saber mas'} />
-                </CardAccions>
-            </Card>
+            <DivRow>
+                <Card>
+                    <CardTitle>Esta es una card de prueba</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Facere pariatur molestiae amet, cupiditate inventore maiores reprehenderit,
+                        ex earum neque ab libero eum quo fuga corporis tempora atque, praesentium ullam nisi.
+                    </CardText>
+                </Card>
+                <Card>
+                    <CardTitle>Card con link</CardTitle>
+                    <CardText>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Inventore commodi numquam nihil ad molestias similique voluptatem
+                        blanditiis ex tempora, accusantium atque. Natus aliquam tempora
+                        dolor cumque architecto dignissimos nesciunt debitis.
+                    </CardText>
+                    <CardAccions>
+                        <CardLink url={'https://stevechambitas.netlify.app/home'} text={'Saber mas'} />
+                    </CardAccions>
+                </Card>
+            </DivRow>
         </div>
     );
 }
