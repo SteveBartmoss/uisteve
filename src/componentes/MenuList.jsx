28ee1bd69f7ea9f9fd62ab5eae651f2dfe523d0c
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-23 13:49:23 -0600

    se corrigio el componente

--- src/componentes/MenuList.jsx
+++ src/componentes/MenuList.jsx
@@ -1,17 +1,19 @@
 import { NavLink } from "react-router-dom";
 
 
-export function MenuList({title,elements}){
-    <div className="menu-list">
-        <div className="menu-list-head">
-            <div className="menu-list-title">{title}</div>
-        </div>
-        <div>
-            {
-                elements.map(item=>
+export function MenuList({ title, elements }) {
+    return (
+        <div className="menu-list">
+            <div className="menu-list-head">
+                <div className="menu-list-title">{title}</div>
+            </div>
+            <div>
+                {
+                    elements.map(item =>
                         <NavLink className="menu-item" to={item.route} >{item.title}</NavLink>
                     )
-            }
+                }
+            </div>
         </div>
-    </div>
+    )
 }
