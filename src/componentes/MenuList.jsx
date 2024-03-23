2c22a5a7118ba112ca446e1a4d15b0e0c36aa4f7
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-23 14:25:14 -0600

    se agrego la logica de el extendible, se agregaron estilos para cerrar y estilos para acomodar el contenido

--- src/componentes/MenuList.jsx
+++ src/componentes/MenuList.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 
 export function MenuList({ title, elements }) {
+
+    const [collapse, setCollapse] = useState(false)
+
+    const onChangeCollapse = () => {
+        setCollapse(!collapse)
+    }
+
     return (
         <div className="menu-list">
             <div className="menu-list-head">
                 <div className="menu-list-title">{title}</div>
+                <img className={collapse ? 'down-icon' : 'up-icon'} src="/Assets/expand.svg" onClick={() => onChangeCollapse()} />
             </div>
-            <div>
+            <div className={collapse ? 'menu-list-open' : 'menu-list-close'} >
                 {
                     elements.map(item =>
                         <NavLink className="menu-item" to={item.route} >{item.title}</NavLink>
