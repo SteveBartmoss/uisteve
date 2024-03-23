0e41245ecd48b3beeaa1cf95a3b0b4515bbc6881
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-23 13:48:49 -0600

    se implemento el componente menu list

--- src/componentes/MenuVertical.jsx
+++ src/componentes/MenuVertical.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { ExtendPanel } from "./ExtendPanel";
+import { MenuList } from "./MenuList";
 
 export function MenuVertical() {
     return (
@@ -7,10 +8,37 @@ export function MenuVertical() {
             <div className="nav-bar">
                 <NavLink className="menu-item" to="/">Home</NavLink>
                 <NavLink className="menu-item" to="/dash">Dashboards</NavLink>
+                <MenuList title={'Cards'} elements={
+                    [
+                        {
+                            title: 'Card Basic',
+                            route: '/cards/basic',
+                        },
+                    ]
+                }
+                />
+                <MenuList title={'Components'} elements={
+                    [
+                        {
+                            title: 'Expansion Panel',
+                            route: '/acordeon',
+                        },
+                    ]
+                }
+                />
                 <ExtendPanel title={'Ui Elements'}>
                     <NavLink className="menu-item" to="/cards/basic">Card Basic</NavLink>
                     <NavLink className="menu-item" to="/acordeon">Expansion Panel</NavLink>
                 </ExtendPanel>
+                <MenuList title={'Forms & Tables'} elements={
+                    [
+                        {
+                            title: 'Texfield',
+                            route: '/forms/texfield',
+                        },
+                    ]
+                }
+                />
                 <NavLink className="menu-item" to="/dash">Fomrs & Tables</NavLink>
                 <NavLink className="menu-item" to="/forms/texfield">Texfield</NavLink>
                 <ExtendPanel title={'Components'}>
@@ -18,6 +46,22 @@ export function MenuVertical() {
                     <NavLink className="menu-item" to="/components/tabs">Tabs</NavLink>
                     <NavLink className="menu-item" to="/components/popup">PopUp</NavLink>
                 </ExtendPanel>
+                <MenuList title={'Components'} elements={
+                    [
+                        {
+                            title: 'Buttons',
+                            route: '/components/buttons',
+                        },
+                        {
+                            title: 'Tabs',
+                            route: '/components/tabs',
+                        },
+                        {
+                            title: 'PopUp',
+                            route: '/components/popup',
+                        }
+                    ]
+                } />
             </div>
         </div>
     );
