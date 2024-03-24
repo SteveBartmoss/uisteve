97ac8dbbbc64dee13a02666d67c177f6a54874af
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-23 18:59:16 -0600

    se cambiaron los titulos de las tabs

---
+++ src/componentes/Tabs.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { DivCol, DivRow } from "./contenedores";
+
+
+export function Tabs({ headers, elements }) {
+
+    const [currentTab, setCurrentTab] = useState(1);
+
+    const onTabChange = (id) => {
+        //console.log('id',id)
+        setCurrentTab(id)
+        //console.log('tab', currentTab)
+    }
+
+    return (
+        <DivCol>
+            <DivRow>
+                {
+                    headers.map(head =>
+                        <div key={head.title} className="header-tabs">
+                            <p onClick={() => onTabChange(head.id)} className={currentTab === head.id ? 'tab tab-actibe' : 'tab'}>{head.title}</p>
+                        </div>
+                    )
+                }
+            </DivRow>
+            {
+                headers.map(head =>
+                    <div key={head.id}>
+                        <div className={currentTab === head.id ? '' : 'tab-close'}>{head.content}</div>
+                    </div>
+                )
+            }
+        </DivCol>
+    )
+}
