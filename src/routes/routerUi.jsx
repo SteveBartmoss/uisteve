841feed93cb9a8434105122f005b45d248cb9763
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-24 16:04:01 -0600

    se agrego un div para tener todo el campo disponible

--- src/routes/routerUi.jsx
+++ src/routes/routerUi.jsx
@@ -1,25 +1,30 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MenuVertical } from "../componentes/MenuVertical";
 import { TextField } from "../componentes/TextField";
 import { BasicCards } from "../pages/BasicCards";
 import { Acordeones } from "../pages/ExpansionPanel";
 import { Buttons } from "../pages/Buttons";
 import { TabsMenu } from "../pages/TabsMenu";
+import { Foter } from "../pages/Foter";
+import { DivFull } from "../componentes/contenedores";
 
 
-export function RuoterUi(){
-    return(
+export function RuoterUi() {
+    return (
         <BrowserRouter>
             <MenuVertical />
-            <Routes>
-                <Route path="/" element={<h1>Home</h1>}  />
-                <Route path="/dash" element={<h1>Dash</h1>}  />
-                <Route path="/forms/texfield" element={<TextField />}  />
-                <Route path="/cards/basic" element={<BasicCards />}  />
-                <Route path="/acordeon" element={<Acordeones />} />
-                <Route path="/components/buttons" element={<Buttons />} />
-                <Route path="/components/tabs" element={<TabsMenu />} />
-            </Routes>
+            <DivFull>
+                <Routes>
+                    <Route path="/" element={<h1>Home</h1>} />
+                    <Route path="/dash" element={<h1>Dash</h1>} />
+                    <Route path="/forms/texfield" element={<TextField />} />
+                    <Route path="/cards/basic" element={<BasicCards />} />
+                    <Route path="/acordeon" element={<Acordeones />} />
+                    <Route path="/components/buttons" element={<Buttons />} />
+                    <Route path="/components/tabs" element={<TabsMenu />} />
+                </Routes>
+                <Foter />
+            </DivFull>
         </BrowserRouter>
     );
 }
