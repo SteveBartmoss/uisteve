99fd46fc57c48c127ae6b9eeb581d3991ff746dd
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-24 21:25:49 -0600

    se agrego una funcion para el click de los botones

--- src/pages/Buttons.jsx
+++ src/pages/Buttons.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { Boton } from "../componentes/Boton";
 import { Card, CardAccions, CardText, CardTitle } from "../componentes/Card";
 import { DivRow } from "../componentes/contenedores";
 
 export function Buttons() {
+
+    const [evento,setEvento]=useState('');
+
+    const onHandleClick=(tipo)=>{
+        setEvento(tipo)
+    }
+
     return (
         <div>
             <DivRow>
@@ -11,11 +19,14 @@ export function Buttons() {
                     <CardText>
                         Un Boton que tiene un diseno simple pero funcional
                     </CardText>
+                    <CardText>
+                        Se presiono un boton tipo {evento}
+                    </CardText>
                     <CardAccions>
-                        <Boton variant='' color={'success'}>success</Boton>
-                        <Boton variant='' color={'info'}>info</Boton>
-                        <Boton variant='' color={'warning'}>warning</Boton>
-                        <Boton variant='' color={'error'}>error</Boton>
+                        <Boton evento={()=>onHandleClick('succes')} variant='' color={'success'}>success</Boton>
+                        <Boton evento={()=>onHandleClick('info')} variant='' color={'info'}>info</Boton>
+                        <Boton evento={()=>onHandleClick('warning')} variant='' color={'warning'}>warning</Boton>
+                        <Boton evento={()=>onHandleClick('error')} variant='' color={'error'}>error</Boton>
                     </CardAccions>
                 </Card>
             </DivRow>
