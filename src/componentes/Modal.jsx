994de9e0b0ba26a733c23b841cbcba2c6849304c
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-27 22:27:42 -0600

    se agrego el prototipo del contenedor del modal

--- src/componentes/Modal.jsx
+++ src/componentes/Modal.jsx
@@ -4,6 +4,9 @@
     return(
         <div>
             <h1>Un simple modal</h1>
+            <div>
+                <h1>Este sera el modal para mostrar informacion</h1>
+            </div>
         </div>
     );
  }
