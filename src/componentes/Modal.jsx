31b1ad179a7f0a35d2d500cec03a0278d556d152
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-28 13:20:13 -0600

    se cambio la logica del componente ya que provocaba un fallo, se agrego una funcion para cerrar el modal desde afuera y se agrego un boton para cerrar, se agrego la funcion de cerrar el modal si se da click fuera del mismo

--- src/componentes/Modal.jsx
+++ src/componentes/Modal.jsx
@@ -1,25 +1,18 @@
 import { useEffect, useState } from "react";
+import { Boton } from "./Boton";
 
- 
 
- export function Modal({children,estado}){
 
-    
-    if(estado){
-        return(
-            <div className="overlay">
-                <h1>Un simple modal</h1>
-                <div className="modal-content">
-                    <h1>Este sera el modal para mostrar informacion</h1>
-                </div>
+export function Modal({ children, estado, close }) {
+
+    return (
+        <div onClick={close} className={estado ? 'overlay' : 'modal-close'}>
+            <div className="modal-content">
+                <h1>Este sera el modal para mostrar informacion</h1>
+                <Boton evento={() => close()} variant='' color={'success'}>Cerrar</Boton>
             </div>
-        )
-    }
-    else{
-        return(
-            <>
-            </>
-        )
-    }
+        </div>
 
- }
+    )
+
+}
