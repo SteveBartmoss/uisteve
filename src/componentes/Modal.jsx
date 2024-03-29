f387ff545194dee39eb46f3b82aac9691f4449a9
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-29 13:17:48 -0600

    se quito el efecto de cerrar el modal pulsado fuera del componente por un bug, se agrego el retorno del children y se quito el boton de cerrar

--- src/componentes/Modal.jsx
+++ src/componentes/Modal.jsx
@@ -5,10 +5,9 @@ import { Boton } from "./Boton";
 export function Modal({ children, estado, close }) {
 
     return (
-        <div onClick={close} className={estado ? 'overlay' : 'modal-close'}>
+        <div className={estado ? 'overlay' : 'modal-close'}>
             <div className="modal-content">
-                <h1>Este sera el modal para mostrar informacion</h1>
-                <Boton evento={() => close()} variant='' color={'success'}>Cerrar</Boton>
+                {children}
             </div>
         </div>
 
