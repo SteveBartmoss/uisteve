5c531352d0eb62b257fabd315fe9931bf1f594e2
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-30 21:18:54 -0600

    se agregaron los estilos de lo pagina

--- src/pages/InputsPage.jsx
+++ src/pages/InputsPage.jsx
@@ -1,23 +1,53 @@
-import { Card, CardContent, CardTitle } from "../componentes/Card";
+import { Card, CardContent, CardText, CardTitle } from "../componentes/Card";
 import { TextField } from "../componentes/TextField";
-import { DivRow, Panel } from "../componentes/contenedores";
+import { DivCol, DivMainContent, DivRow, Panel } from "../componentes/contenedores";
 
 
 export function InputsPage() {
     return (
         <Panel>
-            <h1>Textfield</h1>
-            <DivRow>
 
-                <Card>
-                    <CardTitle>Un simple textfield</CardTitle>
-                    <CardContent>
-                        <DivRow>
-                            <TextField />
-                        </DivRow>
-                    </CardContent>
-                </Card>
-            </DivRow>
+            <DivMainContent>
+
+                <h1 className="main-subtitle">Textfield</h1>
+
+                <p className="main-content">
+                    Los formularios son algo comun en las aplicaciones web 
+                    estos text field le daram buen toque a tu aplicacion 
+                    para llamar la atencion de los usuarios.
+                </p>
+
+                <DivRow>
+                    <DivCol>
+                        <Card>
+                            <CardTitle>Un simple textfield</CardTitle>
+                            <CardText>
+                                Una manera de darle un buen estilo a tus formularios
+                            </CardText>
+                            <CardContent>
+                                <TextField />
+                            </CardContent>
+                        </Card>
+                    </DivCol>
+
+                    <DivCol>
+                        <Card>
+                            <CardTitle>Un formulario simple</CardTitle>
+                            <CardText>
+                                Registre sus datos
+                            </CardText>
+                            <CardContent>
+                                <TextField />
+                            </CardContent>
+                            <CardContent>
+                                <TextField />
+                            </CardContent>
+                        </Card>
+                    </DivCol>
+
+                </DivRow>
+            </DivMainContent>
+
         </Panel>
     );
 }
