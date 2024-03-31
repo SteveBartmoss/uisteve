182f52635895d70ca7373725915ce0831d694e8d
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-31 12:39:01 -0600

    se agregaron los elementos de las pagina principales a las paginas de las tabs

--- src/pages/TabsMenu.jsx
+++ src/pages/TabsMenu.jsx
@@ -1,54 +1,63 @@
 import { Card, CardContent, CardText, CardTitle } from "../componentes/Card";
 import { Tabs } from "../componentes/Tabs";
-import { DivRow, Panel } from "../componentes/contenedores";
+import { DivMainContent, DivRow, Panel } from "../componentes/contenedores";
 
 export function TabsMenu() {
     return (
         <Panel>
-            <h1>Tabs menu</h1>
-            <DivRow>
-                <Card>
-                    <CardTitle>Tabs simples</CardTitle>
-                    <CardText>
-                        Unas simples tabs que permiten cambiar elementos
-                    </CardText>
-                    <CardContent>
-                        <Tabs headers={
-                            [
-                                {
-                                    id: 1,
-                                    title: "Tab1",
-                                    content:
-                                        <Panel>
-                                            <h1>La primera tab</h1>
-                                            <p>Este es el contenido de la primera tab</p>
-                                        </Panel>,
-                                },
-                                {
-                                    id: 2,
-                                    title: "Tab2",
-                                    content:
-                                        <Panel>
-                                            <h1>La segunda tab</h1>
-                                            <p>Este es el contenido de la segunda tab</p>
-                                        </Panel>,
-                                },
-                                {
-                                    id: 3,
-                                    title: "Tab3",
-                                    content:
-                                        <Panel>
-                                            <h1>La tercera tab</h1>
-                                            <p>Este es el contenido de la tercera tab</p>
-                                        </Panel>,
-                                },
-                            ]
-                        }
+            <DivMainContent>
+                <h1 className="main-subtitle">Tabs menu</h1>
+                <p className="main-content">
+                    Una Buena forma de evitar una redireccion es guardando 
+                    todo en una tab para que usuario pued navegart en ellas,
+                    aun esta en progreso no me peguen TT_TT
+                </p>
 
-                        />
-                    </CardContent>
-                </Card>
-            </DivRow>
+                <DivRow>
+                    <Card>
+                        <CardTitle>Tabs simples</CardTitle>
+                        <CardText>
+                            Unas simples tabs que permiten cambiar elementos
+                        </CardText>
+                        <CardContent>
+                            <Tabs headers={
+                                [
+                                    {
+                                        id: 1,
+                                        title: "Tab1",
+                                        content:
+                                            <Panel>
+                                                <h1>La primera tab</h1>
+                                                <p>Este es el contenido de la primera tab</p>
+                                            </Panel>,
+                                    },
+                                    {
+                                        id: 2,
+                                        title: "Tab2",
+                                        content:
+                                            <Panel>
+                                                <h1>La segunda tab</h1>
+                                                <p>Este es el contenido de la segunda tab</p>
+                                            </Panel>,
+                                    },
+                                    {
+                                        id: 3,
+                                        title: "Tab3",
+                                        content:
+                                            <Panel>
+                                                <h1>La tercera tab</h1>
+                                                <p>Este es el contenido de la tercera tab</p>
+                                            </Panel>,
+                                    },
+                                ]
+                            }
+
+                            />
+                        </CardContent>
+                    </Card>
+                </DivRow>
+
+            </DivMainContent>
         </Panel>
     );
 }
