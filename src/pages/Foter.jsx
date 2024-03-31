980aa2ea4dcca71e78688b5d6295da1fa6226f32
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-31 12:13:04 -0600

    se agrego el contenido del footer para la futura implementacion de esa paginas

--- src/pages/Foter.jsx
+++ src/pages/Foter.jsx
@@ -1,9 +1,21 @@
+import { DivCol, DivFooterContent, DivMainContent, DivRow } from "../componentes/contenedores";
 
 
-export function Foter(){
-    return(
+export function Foter() {
+    return (
         <div className="div-footer">
-            <h1>Footer</h1>
+            <DivMainContent>
+                <DivFooterContent>
+                    <DivRow>
+                        <p className="footer-link">Power by DevFarming</p>
+                    </DivRow>
+                    <DivRow>
+                        <p className="footer-link">About</p>
+                        <p className="footer-link">Licencia</p>
+                        <p className="footer-link">Documentacion</p>
+                    </DivRow>
+                </DivFooterContent>
+            </DivMainContent>
         </div>
     );
 }
