5e6f947dc8926a8e5305743bae50eec34c7e8235
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-30 21:18:04 -0600

    se agregaron los estilos de lo pagina

--- src/pages/BasicCards.jsx
+++ src/pages/BasicCards.jsx
@@ -1,51 +1,65 @@
 import { Card, CardAccions, CardImg, CardImgContent, CardLink, CardText, CardTitle } from "../componentes/Card";
-import { DivCol, DivRow, Panel } from "../componentes/contenedores";
+import { DivCol, DivMainContent, DivRow, Panel } from "../componentes/contenedores";
 
 export function BasicCards() {
     return (
         <Panel>
-            <h1>Basic Cards</h1>
-            <DivRow>
+            <DivMainContent>
 
-                <DivCol>
-                    <Card>
-                        <CardTitle>Esta es una card de prueba</CardTitle>
-                        <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Facere pariatur molestiae amet, cupiditate inventore maiores reprehenderit,
-                            ex earum neque ab libero eum quo fuga corporis tempora atque, praesentium ullam nisi.
-                        </CardText>
-                    </Card>
-                </DivCol>
+                <h1 className="main-subtitle">Basic Cards</h1>
 
-                <DivCol>
-                    <Card>
-                        <CardTitle>Card con link</CardTitle>
-                        <CardText>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                            Inventore commodi numquam nihil ad molestias similique voluptatem
-                            blanditiis ex tempora, accusantium atque. Natus aliquam tempora
-                            dolor cumque architecto dignissimos nesciunt debitis.
-                        </CardText>
-                        <CardAccions>
-                            <CardLink url={'https://stevechambitas.netlify.app/home'} text={'Saber mas'} />
-                        </CardAccions>
-                    </Card>
-                </DivCol>
+                <p className="main-content">
+                    Un elemento que esta presente en muchas librerias de ui y que 
+                    aportan un estilo llamativo a una aplicacion web, estas simples 
+                    cards pueden organizar el contenido de una forma rapida.
+                </p>
 
-                <DivCol>
-                    <CardImg urlImg={"montain.jpg"}>
-                        <CardImgContent>
+                <DivRow>
+
+                    <DivCol>
+                        <Card>
+                            <CardTitle>Esta es una card de prueba</CardTitle>
                             <CardText>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                Sunt, obcaecati atque. Placeat officia ducimus fugiat natus harum eos,
-                                a voluptatum cumque est
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Facere pariatur molestiae amet, cupiditate inventore maiores reprehenderit,
+                                ex earum neque ab libero eum quo fuga corporis tempora atque, praesentium ullam nisi.
                             </CardText>
-                        </CardImgContent>
-                    </CardImg>
-                </DivCol>
+                        </Card>
+                    </DivCol>
 
-            </DivRow>
+                    <DivCol>
+                        <Card>
+                            <CardTitle>Card con link</CardTitle>
+                            <CardText>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                Inventore commodi numquam nihil ad molestias similique voluptatem
+                                blanditiis ex tempora, accusantium atque. Natus aliquam tempora
+                                dolor cumque architecto dignissimos nesciunt debitis.
+                            </CardText>
+                            <CardAccions>
+                                <CardLink url={'https://stevechambitas.netlify.app/home'} text={'Saber mas'} />
+                            </CardAccions>
+                        </Card>
+                    </DivCol>
+
+                </DivRow>
+
+                <DivRow>
+                    <DivCol>
+                        <CardImg urlImg={"montain.jpg"}>
+                            <CardImgContent>
+                                <CardText>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Sunt, obcaecati atque. Placeat officia ducimus fugiat natus harum eos,
+                                    a voluptatum cumque est
+                                </CardText>
+                            </CardImgContent>
+                        </CardImg>
+                    </DivCol>
+                </DivRow>
+
+            </DivMainContent>
+
         </Panel>
 
     );
