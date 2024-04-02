9f78b3b3faefac42024d8f3c72f27640385127a2
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-01 21:31:33 -0600

    se agrego la pagina para el contenido de about

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import { DivMainContent, DivRow } from "../componentes/contenedores";
+
+
+export function About(){
+    return(
+        <Panel>
+            <DivRow>
+                <DivMainContent>
+                    <h1>Acerda de Ui Steve</h1>
+                </DivMainContent>
+            </DivRow>
+        </Panel>
+    );
+}
