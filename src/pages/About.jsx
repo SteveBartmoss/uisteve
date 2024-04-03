7fea664d36786d83ccf34b312241bc90dabe7b70
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-02 21:57:43 -0600

    se agrego ma informacion a la pagina about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,12 @@ export function About(){
             <DivRow>
                 <DivMainContent>
                     <h1>Acerda de Ui Steve</h1>
+                    <p>Este proyecto tiene como objetivo presentar una 
+                        plantilla de elementos ui para que los usuarios puedan usarlos 
+                        o aprender como crearlos, al igual que si alguien 
+                        requiere solamente usar una plantilla de front end 
+                        dispones de recursos
+                    </p>
                 </DivMainContent>
             </DivRow>
         </Panel>
