e32fbe9cb94f754699476bd3300c0f3508b6d0f4
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-06 11:48:57 -0600

    se agrego mas contenido a la pagina about'

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import { DivMainContent, DivRow } from "../componentes/contenedores";
+import { DivMainContent, DivRow, Panel } from "../componentes/contenedores";
 
 
 export function About() {
@@ -6,14 +6,14 @@ export function About() {
         <Panel>
             <DivRow>
                 <DivMainContent>
-                    <h1>Acerda de Ui Steve</h1>
-                    <p>Este proyecto tiene como objetivo presentar una
+                    <h1 className="main-subtitle">Acerca de Ui Steve</h1>
+                    <p className="main-content">Este proyecto tiene como objetivo presentar una
                         plantilla de elementos ui para que los usuarios puedan usarlos
                         o aprender como crearlos, al igual que si alguien
                         requiere solamente usar una plantilla de front end
-                        dispones de recursos
-                    </p>s
-                    <p>
+                        dispones de recursos para implementar rapidamente.
+                    </p>
+                    <p className="main-content">
                         Esta plantilla es desarrollada de manera independiente y sin fines de obtener un
                         lucro, esto puede hacer que el desarrollo se vea lento o que no se busque mejorar
                         tan frecuentemente como cualquier otro proyecto que tiene una gran empresa detras.
@@ -22,10 +22,12 @@ export function About() {
                         por lo que esta plantilla no solo pretende ser de ayuda para otras personas, si 
                         no tambien servir de muestra en mi portfolio.
                     </p>
-                    <p>
+                    <p className="main-content">
                         La mayoria de los componentes estas pensados para ser reutilizados de 
                         una forma sencilla y que tambien puedan ser estudiados son tener muchos 
-                        problemas
+                        problemas, ademas se estara trabajando para proporcionar la informacion sobre como utilizar 
+                        los componentes (props que reciben, si pueden ser contenedores o no, que funcion recibe) 
+                        para que se pueda usar sin problemas.
                     </p>
                 </DivMainContent>
             </DivRow>
