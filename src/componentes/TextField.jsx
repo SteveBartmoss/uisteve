ae5d1e242f0feac377f39925362db832bed0b26e
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-05 21:38:59 -0600

    se implemento el uso de las props para el componente

--- src/componentes/TextField.jsx
+++ src/componentes/TextField.jsx
@@ -1,11 +1,11 @@
 import "../App.css"
 
-export function TextField({label,placeholder}) {
+export function TextField({label,ayuda}) {
     return (
         <>
             <div className="form">
-                <input type="text" id="field" className="text-field" placeholder=" " />
-                <label for="field" className="text-field-label">Nombre</label>
+                <input type="text" id="field" className="text-field" placeholder={ayuda} />
+                <label for="field" className="text-field-label">{label}</label>
             </div>
         </>
     );
