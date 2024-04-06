58595b9e32152599cd4ccbaa83db89e261a93a79
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-05 21:39:22 -0600

    se agrego mas contenido para el curso

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,6 +19,13 @@ export function About() {
                         tan frecuentemente como cualquier otro proyecto que tiene una gran empresa detras.
 
                         Otro de los objetivos es mejorar mis habilidades a la hora de desarrollar aplicaciones web
+                        por lo que esta plantilla no solo pretende ser de ayuda para otras personas, si 
+                        no tambien servir de muestra en mi portfolio.
+                    </p>
+                    <p>
+                        La mayoria de los componentes estas pensados para ser reutilizados de 
+                        una forma sencilla y que tambien puedan ser estudiados son tener muchos 
+                        problemas
                     </p>
                 </DivMainContent>
             </DivRow>
