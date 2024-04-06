66e497a1162c7b3b861fe10899b8577cdb288dc5
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-06 11:23:50 -0600

    se quito la propiedad del place holder porque aun no estadel todo bien

--- src/componentes/TextField.jsx
+++ src/componentes/TextField.jsx
@@ -1,10 +1,10 @@
 import "../App.css"
 
-export function TextField({label,ayuda}) {
+export function TextField({label}) {
     return (
         <>
             <div className="form">
-                <input type="text" id="field" className="text-field" placeholder={ayuda} />
+                <input type="text" id="field" className="text-field" placeholder='' />
                 <label for="field" className="text-field-label">{label}</label>
             </div>
         </>
