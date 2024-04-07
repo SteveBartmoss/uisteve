590d05e8b0bc75015255861866ac46a09296c410
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-06 19:36:36 -0600

    se mejoro el contenido de la pagina de tabs

--- src/pages/TabsMenu.jsx
+++ src/pages/TabsMenu.jsx
@@ -1,14 +1,24 @@
-import { Card, CardContent, CardText, CardTitle } from "../componentes/Card";
+import { useState } from "react";
+import { Boton } from "../componentes/Boton";
+import { Card, CardAccions, CardContent, CardText, CardTitle } from "../componentes/Card";
 import { Tabs } from "../componentes/Tabs";
-import { DivMainContent, DivRow, Panel } from "../componentes/contenedores";
+import { TextField } from "../componentes/TextField";
+import { DivCol, DivMainContent, DivRow, Panel, Ventana, VentanaContent } from "../componentes/contenedores";
 
 export function TabsMenu() {
+
+    const [mensaje, setMensaje] = useState('');
+
+    const onPressBoton = () => {
+        setMensaje('boton presionado')
+    }
+
     return (
         <Panel>
             <DivMainContent>
                 <h1 className="main-subtitle">Tabs menu</h1>
                 <p className="main-content">
-                    Una Buena forma de evitar una redireccion es guardando 
+                    Una Buena forma de evitar una redireccion es guardando
                     todo en una tab para que usuario pued navegart en ellas,
                     aun esta en progreso no me peguen TT_TT
                 </p>
@@ -26,34 +36,64 @@ export function TabsMenu() {
                                         id: 1,
                                         title: "Tab1",
                                         content:
-                                            <Panel>
-                                                <h1>La primera tab</h1>
-                                                <p>Este es el contenido de la primera tab</p>
-                                            </Panel>,
+                                            <Ventana>
+                                                <p className="main-content">Datos personales</p>
+                                                <DivRow>
+                                                    <DivCol>
+                                                        <TextField label={"Nombre"} />
+                                                        <TextField label={"Apellidos"} />
+                                                    </DivCol>
+                                                    <DivCol>
+                                                        <TextField label={"Correo"} />
+                                                        <TextField label={"Password"} />
+                                                    </DivCol>
+                                                </DivRow>
+                                            </Ventana>,
                                     },
                                     {
                                         id: 2,
                                         title: "Tab2",
                                         content:
-                                            <Panel>
-                                                <h1>La segunda tab</h1>
-                                                <p>Este es el contenido de la segunda tab</p>
-                                            </Panel>,
+                                            <Ventana>
+                                                <p className="main-content">Datos de contacto</p>
+                                                <DivRow>
+                                                    <DivCol>
+                                                        <TextField label={"Telefono"} />
+                                                        <TextField label={"Celular"} />
+                                                    </DivCol>
+                                                    <DivCol>
+                                                        <TextField label={"Curp"} />
+                                                        <TextField label={"Github"} />
+                                                    </DivCol>
+                                                </DivRow>
+                                            </Ventana>,
                                     },
                                     {
                                         id: 3,
                                         title: "Tab3",
                                         content:
-                                            <Panel>
-                                                <h1>La tercera tab</h1>
-                                                <p>Este es el contenido de la tercera tab</p>
-                                            </Panel>,
+                                            <Ventana>
+                                                <p className="main-content">Datos de domicilio</p>
+                                                <DivRow>
+                                                    <DivCol>
+                                                        <TextField label={"Calle"} />
+                                                        <TextField label={"Fracc"} />
+                                                    </DivCol>
+                                                    <DivCol>
+                                                        <TextField label={"Codigo"} />
+                                                        <TextField label={"Estado"} />
+                                                    </DivCol>
+                                                </DivRow>
+                                            </Ventana>,
                                     },
                                 ]
                             }
 
                             />
                         </CardContent>
+                        <CardAccions>
+                            <Boton evento={onPressBoton} variant='' color={'principal'}>try me</Boton>
+                        </CardAccions>
                     </Card>
                 </DivRow>
 
