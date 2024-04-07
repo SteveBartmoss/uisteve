7ccc9b6e0b3a9ed7395f6e52b0d85c3573b81255
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-06 23:10:16 -0600

    se actualizo la version en la pagina about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -47,7 +47,7 @@ export function About() {
 
                     <hr className="divider" />
 
-                    <h1 className="main-subtitle">Ui Steve 0.2.0 Gemmel</h1>
+                    <h1 className="main-subtitle">Ui Steve 0.3.0 Gemmel</h1>
 
                     <p className="main-content">
                         La plantialla cuenta actualmente con los siguientes elementos:
