f028ee0470cfc1145a4d399fee07081ddd81ad08
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-10 21:36:06 -0600

    se creo una nueva pagina para la documentacion de la plantilla

---
+++ src/pages/Documentacion.jsx
@@ -0,0 +1,10 @@
+import { Panel } from "../componentes/contenedores";
+
+
+export function Documentacion() {
+    return(
+        <Panel>
+            <h1>Documentacion</h1>
+        </Panel>
+    );
+}
