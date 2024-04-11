38b0d09746de4db5e386ef4f9061f773dd4e95f3
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-10 21:36:32 -0600

    se dejo un ejemplo de la implementacion de google icons

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,8 @@ import { Lista } from "../componentes/Lista";
 import { DivMainContent, DivRow, Panel } from "../componentes/contenedores";
 
 
-const listaComponentes=["Botones","Tabs","Modales","TextField","Panel Colapsable","Cards"];
-const listaNuevos=["Plantillas (login,404,landing, etc)","Alertas","Listas","Menus","Paginaciones","Snackbar","Tooltips","Elementos de formularios","Tema oscuro/claro"]
+const listaComponentes = ["Botones", "Tabs", "Modales", "TextField", "Panel Colapsable", "Cards"];
+const listaNuevos = ["Plantillas (login,404,landing, etc)", "Alertas", "Listas", "Menus", "Paginaciones", "Snackbar", "Tooltips", "Elementos de formularios", "Tema oscuro/claro"]
 
 export function About() {
     return (
@@ -15,7 +15,7 @@ export function About() {
                     <p className="main-content">
                         Este proyecto tiene como objetivo presentar una plantilla de elementos
                         ui para que los usuarios puedan usarlos o aprender como crearlos, al igual
-                        que si alguien requiere solamente usar una plantilla de front end dispones
+                        que si alguien requiere solamente usar una plantilla de front end disponer
                         de recursos para implementar rapidamente.
                     </p>
 
@@ -38,10 +38,10 @@ export function About() {
                     </p>
 
                     <p className="main-content">
-                        La mayoria de los componentes estas pensados para ser reutilizados de
-                        una forma sencilla y que tambien puedan ser estudiados son tener muchos
+                        La mayoria de los componentes estan pensados para ser reutilizados de
+                        una forma sencilla y que tambien puedan ser estudiados sin tener muchos
                         problemas, ademas se estara trabajando para proporcionar la informacion sobre como utilizar
-                        los componentes (props que reciben, si pueden ser contenedores o no, que funcion recibe)
+                        los componentes (props que reciben, si pueden ser contenedores o no, que eventos recibe)
                         para que se pueda usar sin problemas.
                     </p>
 
@@ -50,13 +50,13 @@ export function About() {
                     <h1 className="main-subtitle">Ui Steve 0.3.0 Gemmel</h1>
 
                     <p className="main-content">
-                        La plantialla cuenta actualmente con los siguientes elementos:
+                        La plantilla cuenta actualmente con los siguientes elementos:
                     </p>
 
                     <Lista elementos={listaComponentes} />
 
                     <p className="main-content">
-                        Los componentes estan pensado para ser usados al copiar la plantilla
+                        Los componentes estan pensados para ser usados al copiar la plantilla
                         de manera que puedan ser importados como si se tratara de una funcion
                         o de algun hook de react, basta con copiar el repositorio de la plantilla.
                     </p>
@@ -66,17 +66,25 @@ export function About() {
                     <h1 className="main-subtitle">Road map</h1>
 
                     <p className="main-content">
-                        Algunos de los elementos que me interesan integrar a la plantilla son 
+                        Algunos de los elementos que me interesan integrar a la plantilla son
                         los siguientes:
                     </p>
 
                     <Lista elementos={listaNuevos} />
 
                     <p className="main-content">
-                        Estos elementos se agregaran progresivamente con el timepo o 
-                        al menos es lo que espero, pero se podran ver en las proximas 
+                        Estos elementos se agregaran progresivamente con el timepo o
+                        al menos es lo que espero, pero se podran ver en las proximas
                         versiones las novedades de la plantilla.
                     </p>
+
+                    <p>Ejemplo de integracin con google icons</p>
+
+                    <span class="material-icons md-light">home</span>
+                    <span class="material-icons-outlined md-light">
+                        search
+                    </span>
+                    <span class="material-icons md-light">face</span>
 
                 </DivMainContent>
             </DivRow>
