819edd2013d8d7a1c3a7643360829da88be6030c
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-13 14:06:07 -0600

    se agrego una pagina para el ejemplo de los loaders

---
+++ src/pages/LoadaerPage.jsx
@@ -0,0 +1,34 @@
+import { CardText } from "../componentes/Card";
+import { DivCol, DivMainContent, DivRow, Panel } from "../componentes/contenedores";
+
+
+export function LoaderPage() {
+    return (
+        <Panel>
+            <DivMainContent>
+                <h1 className="main-subtitle">Loader</h1>
+
+                <p className="main-content">
+                    No siempre la velocidad es el aliado de
+                    los usuarios pero pueden ver esta simple
+                    animacion cuando carga su contenido
+                </p>
+
+                <DivRow>
+                    <DivCol>
+                        <Card>
+                            <CardTitle>Loader</CardTitle>
+                            <CardText>
+                                Puedes estar seguro que este loader 
+                                nunca terminara
+                            </CardText>
+                            <CardContent>
+                                <Loader />
+                            </CardContent>
+                        </Card>
+                    </DivCol>
+                </DivRow>
+            </DivMainContent>
+        </Panel>
+    )
+}
