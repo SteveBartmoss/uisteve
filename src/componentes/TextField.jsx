c07cdd2a0b8c94fdd21ea6188b100bc03830d1dd
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-13 20:45:26 -0600

    se corrigio un warning de  la aplicacion

--- src/componentes/TextField.jsx
+++ src/componentes/TextField.jsx
@@ -5,7 +5,7 @@ export function TextField({label}) {
         <>
             <div className="form">
                 <input type="text" id="field" className="text-field" placeholder='' />
-                <label for="field" className="text-field-label">{label}</label>
+                <label htmlFor="field" className="text-field-label">{label}</label>
             </div>
         </>
     );
