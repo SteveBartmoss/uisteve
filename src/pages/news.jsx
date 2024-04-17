c668e78adadd91f2f3b6a4fd7e75fd1436f758f7
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-16 21:38:09 -0600

    se agrego informacion a la pagina de novedades

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -4,6 +4,7 @@ import { Panel } from "../componentes/contenedores";
 export function News(){
     return(
         <Panel>
+            <h1>Ui Steve version 0.5.0 Gemmel</h1>
             <h1>Novedades</h1>
         </Panel>
     )
