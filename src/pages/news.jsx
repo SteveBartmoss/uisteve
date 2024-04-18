867130de49dad4af521ed55bb90c6bb37b5b37ab
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-17 21:24:32 -0600

    se agrego mas contenido a la pagina news

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -6,6 +6,19 @@ export function News(){
         <Panel>
             <h1>Ui Steve version 0.5.0 Gemmel</h1>
             <h1>Novedades</h1>
+            <h1>Nuevos loader</h1>
+            <p>
+                Ahora dispones de un simple pero efectivo componente 
+                que puedes mostrar mientras la informacion de tu 
+                aplicacion termina de llegar
+            </p>
+            <h1>Mejoras en las tabs</h1>
+            <p>
+                El componente tabs ahora esta mas complete y permite 
+                mostrar los elementos de una mejor forma, se esperan 
+                proximas mejoras pero por el momento es una buen comienzo
+            </p>
+            <h1>Iconos</h1>
         </Panel>
     )
 }
