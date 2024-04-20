c952c1f1fd36ba579228d06d667be629412d4167
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-20 15:37:43 -0600

    se implemento otro componente que regresa una barra de carga

--- src/componentes/Loader.jsx
+++ src/componentes/Loader.jsx
@@ -5,3 +5,13 @@ export function Loader(){
         <div className="loader"></div>
     )
 }
+
+export function LoaderBar(){
+    return(
+        <div className="loader-bar">
+            <div className="line-box">
+                <div className="line-load"></div>
+            </div>
+        </div>
+    )
+}
