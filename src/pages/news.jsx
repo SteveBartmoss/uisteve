c47d080b1bdffe132d0ef0759ecf8ba24397d6bc
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-20 15:39:08 -0600

    se agregaron estilos a la seccio de novedades

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -1,30 +1,45 @@
-import { Panel } from "../componentes/contenedores";
+import { DivMainContent, DivRow, Panel } from "../componentes/contenedores";
 
 
-export function News(){
-    return(
+export function News() {
+    return (
         <Panel>
-            <h1>Ui Steve version 0.5.0 Gemmel</h1>
-            <h1>Novedades</h1>
-            <h1>Nuevos loader</h1>
-            <p>
-                Ahora dispones de un simple pero efectivo componente 
-                que puedes mostrar mientras la informacion de tu 
-                aplicacion termina de llegar
-            </p>
-            <h1>Mejoras en las tabs</h1>
-            <p>
-                El componente tabs ahora esta mas complete y permite 
-                mostrar los elementos de una mejor forma, se esperan 
-                proximas mejoras pero por el momento es una buen comienzo
-            </p>
-            <h1>Iconos</h1>
-            <p>
-                Gracias a las herramientas de google ahora puedes tener 
-                llamtivos iconos en la plantilla, por el momento la implementacion 
-                es mediante su propio elemento pero se espera poder agregarlos 
-                a otros elementos mediante una prop
-            </p>
+            <DivRow>
+                <DivMainContent>
+                    <h1 className="main-title">Ui Steve version 0.5.0 Gemmel</h1>
+
+                    <hr className="divider" />
+
+                    <h1 className="main-subtitle">Change Log</h1>
+
+                    <h1 className="alter-subtitle">Nuevos loader</h1>
+
+                    <p className="main-content">
+                        Ahora dispones de un simple pero efectivo componente
+                        que puedes mostrar mientras la informacion de tu
+                        aplicacion termina de llegar
+                    </p>
+
+                    <h1 className="alter-subtitle">Mejoras en las tabs</h1>
+
+                    <p className="main-content">
+                        El componente tabs ahora esta mas complete y permite
+                        mostrar los elementos de una mejor forma, se esperan
+                        proximas mejoras pero por el momento es una buen comienzo
+                    </p>
+
+                    <h1 className="alter-subtitle">Iconos</h1>
+
+                    <p className="main-content">
+                        Gracias a las herramientas de google ahora puedes tener
+                        llamtivos iconos en la plantilla, por el momento la implementacion
+                        es mediante su propio elemento pero se espera poder agregarlos
+                        a otros elementos mediante una prop
+                    </p>
+
+                </DivMainContent>
+            </DivRow>
+
         </Panel>
     )
 }
