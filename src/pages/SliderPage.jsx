e72cf44e0f8fd0a2bbdce5f07e83caedf17ba85d
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-20 17:32:47 -0600

    se agrego una pagina para el elemento slider

---
+++ src/pages/SliderPage.jsx
@@ -0,0 +1,38 @@
+import { Card, CardAccions, CardText, CardTitle } from "../componentes/Card";
+import { RangeSlider } from "../componentes/RangeSlider";
+import { DivCol, DivMainContent, DivRow, Panel } from "../componentes/contenedores";
+
+
+export function SliderPage() {
+
+    return (
+        <Panel>
+            <DivMainContent>
+                <h1 className="main-subtitle">Slider</h1>
+
+                <p className="main-content">
+                    Los usuarios pueder aburrirse a la hora de
+                    usar los controles de te aplicacion pero con
+                    este slider encontraran algo llamativo
+                </p>
+
+                <DivRow>
+
+                    <DivCol>
+                        <Card>
+                            <CardTitle>Un simple slider</CardTitle>
+                            <CardText>
+                                Un elemento llamativo paras tu aplicacion
+                            </CardText>
+                            <CardAccions>
+                                <RangeSlider />
+                            </CardAccions>
+                        </Card>
+                    </DivCol>
+
+                </DivRow>
+            </DivMainContent>
+        </Panel>
+    )
+
+}
