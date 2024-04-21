388d43c52430c9b613196f1a9ec4da1a7e3d4548
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-21 12:37:26 -0600

    se agrego una pagina para mostrar un ejemplo del tooltip

---
+++ src/pages/ToolTipPage.jsx
@@ -0,0 +1,38 @@
+import { Boton } from "../componentes/Boton";
+import { Card, CardAccions, CardContent, CardText, CardTitle } from "../componentes/Card";
+import { ToolTip } from "../componentes/ToolTip";
+import { DivCol, DivMainContent, DivRow, Panel } from "../componentes/contenedores";
+
+export function ToolTipPage() {
+    return (
+        <Panel>
+            <DivMainContent>
+                <h1 className="main-subtitle">Tool Tip</h1>
+
+                <p className="main-content">
+                    Las interfaces de usuario no siempre son claras
+                    para todos pero con este tooltip puedes mostrar
+                    algo de ayuda para los usuarios
+                </p>
+
+                <DivRow>
+                    <DivCol>
+                        <Card>
+                            <CardTitle>Ayuda para el usuario</CardTitle>
+                            <CardText>
+                                Esta tarjeta puede ser confusa para el usuarios
+                                pero el tooltip esta aqui para ayudarnos
+                            </CardText>
+
+                            <CardAccions>
+                                <ToolTip text={"Presiona para seguir leyendo"}>
+                                    <Boton color={"principal"}>saber mas</Boton>
+                                </ToolTip>
+                            </CardAccions>
+                        </Card>
+                    </DivCol>
+                </DivRow>
+            </DivMainContent>
+        </Panel>
+    )
+}
