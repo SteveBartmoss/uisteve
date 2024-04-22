a5e235e7bc68f9a43dbf09bfc184581bd31ee68b
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-21 19:38:55 -0600

    se agrego una funcion para manejar el estado del componente text field

--- src/componentes/TextField.jsx
+++ src/componentes/TextField.jsx
@@ -1,10 +1,17 @@
-import "../App.css"
+import { useState } from "react";
 
 export function TextField({label}) {
+
+    const [text,setText] = useState("")
+
+    const handleChange=(event)=>{
+        setText(event.target.value)
+    }
+
     return (
         <>
             <div className="form">
-                <input type="text" id="field" className="text-field" placeholder='' />
+                <input type="text" id="prueba" value={text} onChange={handleChange} className="text-field" placeholder='' />
                 <label htmlFor="field" className="text-field-label">{label}</label>
             </div>
         </>
