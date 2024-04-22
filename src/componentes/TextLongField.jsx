3b3bd4fd9c881283f35aa8d01de1deb702c70054
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-21 20:06:05 -0600

    se agrego un componente para los texte area, teniendo el mismo estilo que los textfield

---
+++ src/componentes/TextLongField.jsx
@@ -0,0 +1,13 @@
+
+
+export function TextLongField(){
+
+    return(
+        <>
+            <div className="form-area">
+                <textarea className="text-long-field" placeholder='' />
+                <label className="text-long-label">Prueba</label>
+            </div>
+        </>
+    )
+}
