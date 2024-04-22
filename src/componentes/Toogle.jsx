b3698f3b9d70f9debb09e91fe265ba28bfa67957
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-21 18:29:29 -0600

    se agrego un nuevo componente que funciona como un switch

---
+++ src/componentes/Toogle.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+
+export function Toogle(){
+    
+    const [activate,setActivate] = useState(false)
+    const [rangeValue, setRangeValue] = useState(0)
+
+    const handleRangeChange=(event)=>{
+        setRangeValue(event.target.value)
+        event.target.value == 0 ? setActivate(false) : setActivate(true)
+    }
+
+    return(
+        <div>
+            <input className="toogle" type="range" min="0" max="1" value={rangeValue} onChange={handleRangeChange} />
+        </div>
+    )
+}
