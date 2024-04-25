d2f2e2b1b03f6006227ac764b8da48a8d0fd00eb
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-24 21:27:02 -0600

    se agrego mas contenido a la pagina de la licencia

---
+++ src/pages/Licencia.jsx
@@ -0,0 +1,16 @@
+
+export function Licencia(){
+    return(
+        <div>
+            <h1>Proyecto de codigo Abierto</h1>
+            <p>
+                Este proyecto se considera como parte 
+                del movimiento del opensourde por lo cual 
+                esta bajo la licencia de GPL con lo cual 
+                pudes estar tranquilo al usar esta plantilla y 
+                tambien tiene acceso al codigo fuente de la misma 
+                en el repositorio de github
+            </p>
+        </div>
+    )
+}
