5fa0d8211e0164bf08c5dbedb92b6ac96bad08d5
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-27 12:22:57 -0600

    se agregraron elementos para la pagina del swithc

--- src/pages/TooglePage.jsx
+++ src/pages/TooglePage.jsx
@@ -1,6 +1,35 @@
+import { Card, CardAccions, CardContent, CardText, CardTitle } from "../componentes/Card";
+import { Toogle } from "../componentes/Toogle";
+import { DivCol, DivMainContent, DivRow, Panel } from "../componentes/contenedores";
 
-export function TooglePage(){
-    return(
-        <h1>TooglePage</h1>
+export function TooglePage() {
+    return (
+        <Panel>
+            <DivMainContent>
+                <h1 className="main-subtitle">Switch</h1>
+                <p className="main-content">
+                    Un elemeto simple que puede apagar y prenderse
+                    todas las veces que quieras sin tener que procuparte
+                    por que se rompa
+                </p>
+
+                <DivRow>
+                    <DivCol>
+                        <Card>
+                            <CardTitle>Switch</CardTitle>
+                            <CardText>
+                                Un simple interruptor para mostrar
+                                a tus usuarios
+                            </CardText>
+                            <CardAccions>
+                                <Toogle />
+                            </CardAccions>
+                        </Card>
+                    </DivCol>
+                </DivRow>
+
+            </DivMainContent>
+        </Panel>
+
     )
 }
