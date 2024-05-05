ba5de8021bbc17f9edacc70a5629d1fadc4f02d9
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-04 19:51:10 -0600

    se agrego un componente para el menu en la version mobil

---
+++ src/componentes/MenuMobil.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { Icon } from "./Icon";
+import { MenuList } from "./MenuList";
+
+
+export function MenuMobil() {
+
+    const [abrir, setAbrir] = useState(true)
+
+    const onHandleClick=()=>{
+        setAbrir(!abrir)
+    }
+
+    return (
+        <div className="menu-mobil">
+            <div onClick={onHandleClick}>
+                <Icon icono="menu" color="md-light" />
+            </div>
+            <div className="" >
+                <MenuList title={'Cards'} elements={
+                    [
+                        {
+                            title: 'Card Basic',
+                            route: '/cards/basic',
+                        },
+                    ]
+                }
+                />
+                <MenuList title={'Components'} elements={
+                    [
+                        {
+                            title: 'Expansion Panel',
+                            route: '/acordeon',
+                        },
+                        {
+                            title: 'Buttons',
+                            route: '/components/buttons',
+                        },
+                        {
+                            title: 'Tabs',
+                            route: '/components/tabs',
+                        },
+                        {
+                            title: 'Modales',
+                            route: '/components/modales',
+                        },
+                        {
+                            title: 'Iconos',
+                            route: '/components/icons',
+                        },
+                        {
+                            title: 'Loader',
+                            route: '/components/loader',
+                        },
+                        {
+                            title: 'Slider',
+                            route: '/components/slider',
+                        },
+                        {
+                            title: 'Tool Tip',
+                            route: '/components/tooltip',
+                        },
+                        {
+                            title: 'Chip',
+                            route: '/components/chip',
+                        },
+                        {
+                            title: 'SnackBar',
+                            route: '/components/snackbar',
+                        },
+                        {
+                            title: 'Menu',
+                            route: '/components/menu',
+                        }
+                    ]
+                }
+                />
+                <MenuList title={'Forms & Tables'} elements={
+                    [
+                        {
+                            title: 'Texfield',
+                            route: '/forms/texfield',
+                        },
+                        {
+                            title: 'Switch',
+                            route: '/forms/switch',
+                        },
+                        {
+                            title: 'Slider',
+                            route: '/forms/slider',
+                        },
+                        {
+                            title: 'Text Long',
+                            route: '/forms/textlong',
+                        }
+                    ]
+                }
+                />
+            </div>
+        </div>
+    )
+}
