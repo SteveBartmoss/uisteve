7f020e298129aff5646d18acd0502e4852a11a91
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-04 18:29:29 -0600

    se cambio la funcion de abrir el menu al div head del menu

--- src/componentes/MenuList.jsx
+++ src/componentes/MenuList.jsx
@@ -12,9 +12,9 @@ export function MenuList({ title, elements }) {
 
     return (
         <div className="menu-list">
-            <div className="menu-list-head">
+            <div className="menu-list-head" onClick={() => onChangeCollapse()}>
                 <div className="menu-list-title">{title}</div>
-                <img className={collapse ? 'down-icon' : 'up-icon'} alt="arrow" src="/Assets/expand.svg" onClick={() => onChangeCollapse()} />
+                <img className={collapse ? 'down-icon' : 'up-icon'} alt="arrow" src="/Assets/expand.svg"/>
             </div>
             <div className={collapse ? 'menu-list-open' : 'menu-list-close'} >
                 {
