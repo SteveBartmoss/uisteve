10dcbf03f68b73069a6b6f347ee97b2be7b48c92
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-11 11:14:29 -0600

    se agrego la pagina de ejemplo de la tabla

---
+++ src/pages/TablePage.jsx
@@ -0,0 +1,71 @@
+import { Card, CardContent, CardText, CardTitle } from "../componentes/Card";
+import { DataTable } from "../componentes/DataTable";
+import { DivCol, DivMainContent, DivRow } from "../componentes/contenedores";
+
+
+export function TablePage() {
+
+    const encabezados=[
+        {
+            title: 'Nombre',
+            key: 'name'
+        },
+        {
+            title: 'Apodo',
+            key: 'alias'
+        },
+        {
+            title: 'Telefono',
+            key: 'telefono'
+        }
+    ]
+
+    const elementos=[
+        {
+            name: 'Marco',
+            alias: 'Steve',
+            telefono: '3311892077'
+        },
+        {
+            name: 'Antonio',
+            alias: 'Tony',
+            telefono: '3311892077'
+        },
+        {
+            name: 'Pedro',
+            alias: 'Peter',
+            telefono: '3311892077'
+        }
+    ]
+
+    return (
+        <Panel>
+            <DivMainContent>
+                <h1 className="main-subtitle">Table Simple</h1>
+
+                <p className="main-content">
+                    Los datos pueden ser dificiles de 
+                    mostrar ya que a veces pueden estar 
+                    bastante desordanados, esta tabla permite 
+                    ordenarlos de una manera facil
+                </p>
+
+                <DivRow>
+                    <DivCol>
+                        <Card>
+                            <CardTitle>Tabla simple</CardTitle>
+                            <CardText>
+                                Una tabla para dar orden a tus datos 
+                                y que los usuarios sepan  que es lo que 
+                                estan viendo
+                            </CardText>
+                            <CardContent>
+                                <DataTable headers={encabezados} elements={elementos} /> 
+                            </CardContent>
+                        </Card>
+                    </DivCol>
+                </DivRow>
+            </DivMainContent>
+        </Panel>
+    )
+}
