570d08e934d77aeee069993bc00d0dca512a4b80
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-05-26 13:03:55 -0600

    se agregaron elementos a la plantilla del login

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,9 +1,28 @@
-import { Panel } from "../componentes/contenedores";
+import { Boton } from "../componentes/Boton";
+import { Card, CardContent, CardText, CardTitle } from "../componentes/Card";
+import { TextField } from "../componentes/TextField";
+import { DivRow, Panel } from "../componentes/contenedores";
 
 export function Login(){
     return(
         <Panel>
-            <h1>login XD</h1>
+            <DivRow>
+                <img className="img-login" src="/Assets/montain.jpg" />
+                <Card>
+                    <CardTitle>Bienvenido a UiSteve</CardTitle>
+                    <CardText>
+                        Inicia Sesion para ver la nueva plantilla
+                    </CardText>
+                    <CardContent>
+                        <TextField />
+                        <TextField />
+                        <Boton color={'principal'}>Login</Boton>
+                    </CardContent>
+                    <CardText>
+                        Aun no tiene una cuenta, crea una
+                    </CardText>
+                </Card>
+            </DivRow>
         </Panel>
         
     )
