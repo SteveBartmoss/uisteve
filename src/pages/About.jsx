df91fbd42d42a369de025ecc4385bda9c80c2df2
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-09 12:38:58 -0600

    se cambio la version de la plantilla

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -47,7 +47,7 @@ export function About() {
 
                     <hr className="divider" />
 
-                    <h1 className="main-subtitle">Ui Steve 0.15.7 Gemmel</h1>
+                    <h1 className="main-subtitle">Ui Steve 0.15.8 Gemmel</h1>
 
                     <p className="main-content">
                         La plantilla cuenta actualmente con los siguientes elementos:
