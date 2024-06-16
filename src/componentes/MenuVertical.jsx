cfefffa29f36aa594ad26b2bea292aa085f767d5
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-16 12:18:37 -0600

    se agrego una propiedad para que el menu sea dinamico y se crea con un arreglo de objetos

--- src/componentes/MenuVertical.jsx
+++ src/componentes/MenuVertical.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Icon } from "./Icon";
 import { MenuBoton } from "./MenuBoton";
 
-export function MenuVertical() {
+export function MenuVertical({menuItems}) {
 
     const [abrir, setAbrir] = useState(true)
 
@@ -16,105 +16,11 @@ export function MenuVertical() {
         <div className={abrir ? 'menu-vertical' : 'menu-vertical-close'}>
             <MenuBoton evento={closeMenu} isOpen={abrir} />
             <div className={abrir ? 'nav-bar' : 'nav-bar-close'}>
-
-                <NavLink className="menu-item" to='/'>Home</NavLink>
-                <MenuList title={'Cards'} elements={
-                    [
-                        {
-                            title: 'Card Basic',
-                            route: '/cards/basic',
-                        },
-                    ]
+                {
+                    menuItems.map(item =>
+                        item.route != '' ? <NavLink className="menu-item" to={item.route}>{item.title}</NavLink> : item.childs.length >0 ? <MenuList title={item.title} elements={item.childs} /> : <></>
+                    )
                 }
-                />
-                <MenuList title={'Components'} elements={
-                    [
-                        {
-                            title: 'Expansion Panel',
-                            route: '/acordeon',
-                        },
-                        {
-                            title: 'Buttons',
-                            route: '/components/buttons',
-                        },
-                        {
-                            title: 'Tabs',
-                            route: '/components/tabs',
-                        },
-                        {
-                            title: 'Modales',
-                            route: '/components/modales',
-                        },
-                        {
-                            title: 'Iconos',
-                            route: '/components/icons',
-                        },
-                        {
-                            title: 'Loader',
-                            route: '/components/loader',
-                        },
-                        {
-                            title: 'Tool Tip',
-                            route: '/components/tooltip',
-                        },
-                        {
-                            title: 'Chip',
-                            route: '/components/chip',
-                        },
-                        {
-                            title: 'SnackBar',
-                            route: '/components/snackbar',
-                        },
-                        {
-                            title: 'Menu',
-                            route: '/components/menu',
-                        },
-                        {
-                            title: 'Carrusel',
-                            route: '/components/carrusel',
-                        }
-                    ]
-                }
-                />
-                <MenuList title={'Forms'} elements={
-                    [
-                        {
-                            title: 'Texfield',
-                            route: '/forms/texfield',
-                        },
-                        {
-                            title: 'Switch',
-                            route: '/forms/switch',
-                        },
-                        {
-                            title: 'Slider',
-                            route: '/forms/slider',
-                        },
-                        {
-                            title: 'Text Long',
-                            route: '/forms/textlong',
-                        }
-                    ]
-                }
-                />
-                <MenuList title={'Tables'} elements={
-                    [
-                        {
-                            title: 'Table',
-                            route: '/tables/table',
-                        },
-                    ]
-                }
-                />
-                <MenuList title={'Pages'} elements={
-                    [
-                        {
-                            title: 'Login',
-                            route: '/pages/login',
-                        },
-                    ]
-                }
-                />
             </div>
         </div>
     );
