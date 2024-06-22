fdfb7d3d5361f42cb4dd4120d8c2455593768cae
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-22 10:43:40 -0600

    se agrego una propiedad para que las opciones se agregue de forma dinamica por el usario

--- src/componentes/SelectBox.jsx
+++ src/componentes/SelectBox.jsx
@@ -1,13 +1,14 @@
 import './componentes.css';
 
-export function SelectBox({label}) {
+export function SelectBox({label,items}) {
     return (
         <div className='form-select-box'>
             <select className='select-box'>
-                <option value="Selecciona">Selecciona una opcion</option>
-                <option value="Steve">Steve</option>
-                <option value="Enrique">Enrique</option>
-                <option value="Kevin">Kevin</option>
+                {
+                    items.map(item =>
+                        <option value={item.value}>{item.title}</option>
+                    )
+                }
             </select>
             <span className='select-box-label'>{label}</span>
         </div>
