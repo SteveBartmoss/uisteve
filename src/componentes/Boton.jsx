8bcff41567cafb7d37d862befca290f2878e4d17
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-23 11:08:03 -0600

    se agrego un valor po defecto al color del boton

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,6 +1,6 @@
 import './componentes.css';
 
-export function Boton({children,variant,color,evento}){
+export function Boton({children,variant,color='principal',evento}){
 
     return(
         <button onClick={evento} className={`btn-basic ${variant} ${color}`} >{children}</button>
