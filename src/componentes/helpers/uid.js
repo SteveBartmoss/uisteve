3541ecf3bf6d60f95d7edd0db54d9420484f8255
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-23 17:53:58 -0600

    se implemento una tercera funcion para los id unicos

--- src/componentes/helpers/uid.js
+++ src/componentes/helpers/uid.js
@@ -11,3 +11,16 @@ export function generateUniqueIdV2(){
      // Une las partes para formar un ID único
      return `${timestamp}-${randomPart}-${extraRandomPart}`;
 }
+
+export function generateUniqueIdV3() {
+    const timestamp = Date.now().toString(36);
+    const randomPart = Math.floor(Math.random() * 1000000001).toString(36);
+
+    // Genera un valor criptográfico (si está disponible)
+    const cryptoArray = new Uint32Array(1);
+    window.crypto.getRandomValues(cryptoArray);
+    const cryptoPart = cryptoArray[0].toString(36);
+
+    // Combina las partes para formar un ID único
+    return `${timestamp}-${randomPart}-${cryptoPart}`;
+}
