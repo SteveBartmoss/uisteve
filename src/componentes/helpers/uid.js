143d7df2ea0d7c17812c0db58a173fa0d3b96004
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-23 17:47:45 -0600

    se agrego otra funcion para generar los id unicos

--- src/componentes/helpers/uid.js
+++ src/componentes/helpers/uid.js
@@ -1,3 +1,13 @@
 export function generateUniqueId(){
     return Date.now().toString(36) + Math.random.toString(36).substring(2)
 }
+
+export function generateUniqueIdV2(){
+     // Agrega más entropía combinando diferentes partes
+     const timestamp = Date.now().toString(36);
+     const randomPart = Math.random().toString(36).substr(2, 9);
+     const extraRandomPart = Math.random().toString(36).substr(2, 9);
+ 
+     // Une las partes para formar un ID único
+     return `${timestamp}-${randomPart}-${extraRandomPart}`;
+}
