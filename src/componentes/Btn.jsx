47a22bfb5a1f475caacd5ecfc20390f408cbb3b0
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-29 13:33:18 -0600

    se renombro el componente

---
+++ src/componentes/Btn.jsx
@@ -0,0 +1,8 @@
+import './componentes.css';
+
+export function Btn({children,variant,color='principal',evento}){
+
+    return(
+        <button onClick={evento} className={`btn-basic ${variant} ${color}`} >{children}</button>
+    );
+}
