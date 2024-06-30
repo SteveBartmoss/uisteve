161d4bbdacb54ec513bf0642fc41dd22e35d5563
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-30 10:44:15 -0600

    se agrego la documentacion del boton

--- src/pages/Documentacion.jsx
+++ src/pages/Documentacion.jsx
@@ -6,6 +6,8 @@ export function Documentacion() {
     
     const listaSrc = ["componentes", "pages", "routes"];
 
+    const propsBtn = ["children","color","evento"];
+
     return(
         <Panel>
             <DivMainContent>
@@ -130,6 +132,32 @@ export function Documentacion() {
                     La meta es lograr completar la plantilla y posteriormente convertilar en una libreria 
                     funcional que facilite el trabajo de los desarrolladores a la hora enfrentar la 
                     creacion de una aplicacion web.
+                </p>
+
+                <h1 className="main-subtitle">Componentes</h1>
+
+                <h1>Boton</h1>
+
+                <p className="main-content">
+                    El componente esta creado con las siguientes props:
+                </p>
+
+                <Lista elementos={propsBtn} />
+
+                <p className="main-content">
+                    La propiedad children permite que el componente sea usaso con una etiqueta 
+                    de apertura y de cierre siendo el contenido de la etiqueta lo que se muestra 
+                    en el boton.
+                </p>
+
+                <p className="main-content">
+                    Color define que color usara el boton siendo 5 colores posibles (primary, success, 
+                    info, warning, error) esto son los colores posibles que se le pueden asignar.
+                </p>
+
+                <p className="main-content">
+                    Evento es la funcion que se ejecutara al momento de hacer click en el boton 
+                    puediendo manejar de esta manera todo tipo de acciones.
                 </p>
 
             </DivMainContent>
