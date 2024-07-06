f4d849c8bef806308f4ec65d64dbdc115826ae88
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-06 11:11:42 -0600

    se agrego un decorador de los tokens para el momento de pintar el codigo

---
+++ src/componentes/codeExample/decorator/gogh.js
@@ -0,0 +1,22 @@
+
+function tokenDivider(sourdeCode){
+
+    let words=sourdeCode.split(' ')
+    return words
+
+}
+
+function tokeDecorator(sourceCode){
+
+    let codeObjects=[]
+
+    function saramago(element,index,array){
+        
+    }
+
+    
+}
+
+let codeExample= '<button onClick = { evento } className = { `btn-basic ${variant} ${color}` } > { children } </button>'
+
+console.log(tokenDivider(codeExample))
