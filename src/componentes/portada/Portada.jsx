f247230f1e455a0ef5aff68b99b824949d437f2f
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 13:24:32 -0600

    se movio el componente a una nueva ubicacion

---
+++ src/componentes/portada/Portada.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DivCol, DivRow } from "../contenedores";
+
+export function Portada() {
+    return (
+        <section>
+            <DivRow>
+                <div>
+                    <img />
+                </div>
+                <DivCol>
+                    <h1 >Steve</h1>
+                    <p >Estudiante, programador, amante de los videjuegos</p>
+                </DivCol>
+            </DivRow>
+        </section>
+    )
+}
