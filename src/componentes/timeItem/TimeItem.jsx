001f1301c34c2c164bde2230ab5a42f512a70edb
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-28 17:40:34 -0600

    se agregaron clases para el estilo del time item

--- src/componentes/timeItem/TimeItem.jsx
+++ src/componentes/timeItem/TimeItem.jsx
@@ -1,18 +1,18 @@
 import './timeItem.css';
 
-export function TimeItem({ fecha, texto }) {
+export function TimeItem({ fecha, texto, autor }) {
     return (
-        <div>
+        <div className='div-time-item'>
             <div className='head-time-item'>
                 <div>
                     <img className='img-time-item' src="/Assets/steveA.png" />
                 </div>
-                <div>
+                <div className='div-date-item'>
+                    <h1>{autor}</h1>
                     <p>{fecha}</p>
                 </div>
-                
             </div>
-            <div>
+            <div className='div-text-item'>
                 <p>{texto}</p>
             </div>
         </div>
