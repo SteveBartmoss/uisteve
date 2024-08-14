53ccdacc8056fb7659f2a3477e6378b2897b3db2
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-14 08:15:32 -0600

    Create Contenedores.jsx
    Se agrego el contenido de contenedores a su propio archivo en la carpeta componentes

---
+++ src/componentes/contenedores/Contenedores.jsx
@@ -0,0 +1,79 @@
+export function MainFrame({children}){
+    return(
+        <div className="main-frame">
+            {children}
+        </div>
+    );
+}
+
+export function DivRow({children}){
+    return(
+        <div className="div-row">
+            {children}
+        </div>
+    );
+}
+
+export function DivCol({children}){
+    return(
+        <div className="div-col">
+            {children}
+        </div>
+    );
+}
+
+export function DivFull({children}){
+    return(
+        <div className="div-full">
+            {children}
+        </div>
+    );
+}
+
+export function Panel({children}){
+    return(
+        <div className="div-90">
+            {children}
+        </div>
+    )
+}
+
+export function DivMainContent({children}){
+    return(
+        <div className="div-main-content">
+            {children}
+        </div>
+    )
+}
+
+export function DivFooterContent({children}){
+    return(
+        <div className="div-footer-content">
+            {children}
+        </div>
+    )
+}
+
+export function Ventana({children}){
+    return(
+        <div className="div-window">
+            {children}
+        </div>
+    )
+}
+
+export function DivCode({children}){
+    return(
+        <div className="div-code">
+            {children}
+        </div>
+    )
+}
+
+export function DivCanva({children}){
+    return(
+        <div className="div-canva">
+            {children}
+        </div>
+    )
+}
