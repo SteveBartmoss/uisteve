0e825c42c1c71df504019e0b0488d5ce603948d6
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-15 08:18:53 -0600

    Create Loader.jsx
    Se creo una archivo para los loader en la nueva estructura del proyecto

---
+++ src/componentes/loader/Loader.jsx
@@ -0,0 +1,15 @@
+export function Loader(){
+    return(
+        <div className="loader"></div>
+    )
+}
+
+export function LoaderBar(){
+    return(
+        <div className="loader-bar">
+            <div className="line-box">
+                <div className="line-load"></div>
+            </div>
+        </div>
+    )
+}
