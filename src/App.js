ff5a1fa49fa2af457743a96634bfb261a6fe54cf
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-24 15:34:55 -0600

    se cambio el componente router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import './componentes/css/components.css'
 import { MainFrame } from './componentes/contenedores';
 import { RuoterUi } from './routes/routerUi';
+import { RouterMain } from './routes/routerMain';
 
 function App() {
   return (
     <MainFrame>
-      <RuoterUi />
+      <RouterMain />
     </MainFrame>
   );
 }
