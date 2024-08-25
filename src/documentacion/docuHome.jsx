941ad9ab477d81cd47fc566ce472e682cddff9c4
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-25 12:33:48 -0600

    docs: Se agrego contenido para la pagina principal de la documentacion

--- src/documentacion/docuHome.jsx
+++ src/documentacion/docuHome.jsx
@@ -11,9 +11,26 @@ export function DocuHome() {
                     <p className="main-content">Gracias por usar la plantia o pensar en usarla :)</p>
 
                     <hr className="divider" />
+
+                    <h1 className="main-subtitle">Primeros pasos</h1>
+
+                    <p className="main-content">
+                        Aunque el proyecto esta pensado para llegar a ser usado
+                        como si fuera un paquete de npm (como moment.js, nodemon.js
+                        o pdf.js) aun no esta en un estado completo y no me parece
+                        tan util creala, se pueden usar los componentes de la plantilla
+                        clonando el repositorio y modificando la misma para tus necesasidades.
+                    </p>
+
+                    <p className="main-content">
+                        Puedes clonar el repositorio en el siguiente enlace
+                    </p>
+
+                    <a className="footer-link" rel="noopener noreferrer" target="_blank" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman/uisteve">
+                        Keep Coding
+                    </a>
                 </DivMainContent>
             </Panel>
-            <h1>Documentacion</h1>
         </>
     )
 }
