1bbb96b0f88dd0ea1049f25b275f94c189b5d64b
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-25 12:23:59 -0600

    feat(routerMain): Se agrego una ruta para la documentacion

--- src/routes/routerMain.jsx
+++ src/routes/routerMain.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RuoterUi } from "./routerUi";
 import { Login } from "../layouts/login";
+import { RouterDocu } from "./routerDocu";
 
 
 export function RouterMain() {
@@ -9,6 +10,7 @@ export function RouterMain() {
             <Routes>
                 <Route path="/*" element={<RuoterUi />} />
                 <Route path="login" element={<Login />} />
+                <Route path="/documentation*" element={<RouterDocu />}/>
             </Routes>
         </BrowserRouter>
     )
