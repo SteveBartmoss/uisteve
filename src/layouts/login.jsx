89b2da1063ddfdf7e0391f85b42561bbc9efc62d
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-01 09:41:50 -0600

    feat(style): Se retiraron elementos para que la imagen tome mas espacio en el login

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -5,21 +5,18 @@ import { DivCanva, DivCode, DivCol, DivRow, Panel } from "../componentes/contene
 
 export function Login() {
     return (
-        <Panel>
-            <DivRow>
-                <img className="img-login" src="/Assets/montain.jpg" />
-                <DivCanva>
-                    <DivCol>
-                        <h1 className="alter-subtitle">Bienvenido a UiSteve</h1>
-                        <p className="main-content">Inicia Sesion para ver la nueva plantilla</p>
-                        <TextField label={"Usuario"} />
-                        <TextField label={"Password"} isPass={true} />
-                        <Btn color={'principal'}>Login</Btn>
-                        <p className="main-content">Aun no tiene una cuenta, crea una</p>
-                    </DivCol>
-                </DivCanva>
-            </DivRow>
-        </Panel>
-
+        <DivRow>
+            <img className="img-login" src="/Assets/montain.jpg" />
+            <DivCanva>
+                <DivCol>
+                    <h1 className="alter-subtitle">Bienvenido a UiSteve</h1>
+                    <p className="main-content">Inicia Sesion para ver la nueva plantilla</p>
+                    <TextField label={"Usuario"} />
+                    <TextField label={"Password"} isPass={true} />
+                    <Btn color={'principal'}>Login</Btn>
+                    <p className="main-content">Aun no tiene una cuenta, crea una</p>
+                </DivCol>
+            </DivCanva>
+        </DivRow>
     )
 }
