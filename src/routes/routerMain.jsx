28b30d97e46cfe8126ef34961d3212b9fcf6f6a9
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-14 17:54:14 -0600

    feat(routerMain): Se agrego una ruta para la pagina no encontrada

--- src/routes/routerMain.jsx
+++ src/routes/routerMain.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RuoterUi } from "./routerUi";
 import { Login } from "../layouts/login";
 import { RouterDocu } from "./routerDocu";
+import { BadRoute } from "../layouts/badroute";
 
 
 export function RouterMain() {
@@ -10,6 +11,7 @@ export function RouterMain() {
             <Routes>
                 <Route path="/*" element={<RuoterUi />} />
                 <Route path="login" element={<Login />} />
+                <Route path="badroute" element={<BadRoute />} />
                 <Route path="/documentation/*" element={<RouterDocu />}/>
             </Routes>
         </BrowserRouter>
