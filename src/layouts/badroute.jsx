7dd14189fe28dcfcda845dbb291fa3369a3f530d
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-14 17:43:25 -0600

    style(badroute): Se agregaron elementos de pruebas

--- src/layouts/badroute.jsx
+++ src/layouts/badroute.jsx
@@ -4,6 +4,9 @@ export function BadRoute(){
     return(
         <>
             <h1>Bad Ruute</h1>
+            <p>
+                Hola a todos, como estan
+            </p>
         </>
     )
 }
