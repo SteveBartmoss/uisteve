08b6c927c18b26795a8b50480a2a33b8bffb4095
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-21 21:22:44 -0600

    feat(TextField): Se creo un nuevo componente para el textfield y tambien se probo

---
+++ src/componentes/textField/TextField.jsx
@@ -0,0 +1,13 @@
+import './TextField.css';
+
+export function TextField({label,isPass,text,action}) {
+
+    return (
+        <>
+            <div className="form">
+                <input type={isPass ? 'password' : 'text'} id="prueba" value={text} onChange={(event)=>action(event)} className="text-field" placeholder='' />
+                <label htmlFor="field" className="text-field-label">{label}</label>
+            </div>
+        </>
+    );
+}
