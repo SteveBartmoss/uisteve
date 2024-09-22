4a01050f1e5f4bff6ee5d09f007daf073c0a8865
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-22 16:01:50 -0600

    Style(App): Se limpio el codigo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import './componentes/css/components.css'
 import { MainFrame } from './componentes/contenedores';
-import { RuoterUi } from './routes/routerUi';
 import { RouterMain } from './routes/routerMain';
 
 function App() {
