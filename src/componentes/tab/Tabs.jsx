3a1f515a179a4f7da012649a6204edbaf67fdcbb
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-28 13:53:35 -0600

    refactor: Se agrego un archivo independiente para el componente tabs

---
+++ src/componentes/tab/Tabs.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { DivCol, DivRow } from "./contenedores";
+import './Tabs.css';
+
+export function Tabs({ headers, elements, position="" }) {
+
+    const [currentTab, setCurrentTab] = useState(1);
+
+    const onTabChange = (id) => {
+        setCurrentTab(id)
+    }
+
+    return (
+        <DivCol>
+            <div className={`container-head ${position}`} >
+                {
+                    headers.map(head =>
+                        <div key={head.title} className="header-tabs">
+                            <p onClick={() => onTabChange(head.id)} className={currentTab === head.id ? 'tab tab-actibe' : 'tab'}>{head.title}</p>
+                        </div>
+                    )
+                }
+            </div>
+            <DivRow>
+                {
+                    headers.map(head =>
+                        <div key={head.id} className={currentTab === head.id ? 'container-content' : 'tab-close'}>
+                            {head.content}
+                        </div>
+                    )
+                }
+            </DivRow>
+        </DivCol>
+    )
+}
