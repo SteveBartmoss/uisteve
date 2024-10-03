44ffbe1f56ed226624958fd58a55dc7be4cd72b3
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-10-03 14:39:34 -0600

    feat: Se agregaron los elementos que son requeridos para el componente

---
+++ src/componentes/snackBar/snackBar.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react"
+
+
+export function SnackBar({children,open,setOpen}){
+
+    useEffect(()=>{
+        if(open){
+            const close=setTimeout(()=>{
+                setOpen(false)
+            },2000)
+            return () => clearTimeout(close)
+        }
+    },[open])
+
+    return(
+        <div className={`${open == true ? 'snack-open':'snackbar'}`}>
+            {children}
+        </div>
+    )
+
+}
