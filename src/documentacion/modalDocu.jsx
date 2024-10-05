5264b5ede8ee67b97d8f92b89c601c9b73f86491
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-10-05 13:15:01 -0600

    feat: Se agrego el contenido de la documentacion para el modal

--- src/documentacion/modalDocu.jsx
+++ src/documentacion/modalDocu.jsx
@@ -4,13 +4,13 @@ import { Lista } from "../componentes/Lista";
 
 export function ModalDocu() {
 
-    const propsModal = ["children", "color", "evento"];
+    const propsModal = ["children", "estado", "close", "persistent"];
 
     return (
         <>
             <Panel>
                 <DivMainContent>
-                    <h1 className="main-subtitle">Boton</h1>
+                    <h1 className="main-subtitle">Modal</h1>
 
                     <h1 className="key-word">Props</h1>
 
@@ -24,23 +24,34 @@ export function ModalDocu() {
 
                     <p className="main-content">
                         La propiedad children permite que el componente sea usaso con una etiqueta
-                        de apertura y de cierre siendo el contenido de la etiqueta lo que se muestra
-                        en el boton.
+                        de apertura y de cierre siendo el contenido de la etiqueta lo que se muestra 
+                        dentro del modal.
                     </p>
 
-                    <h1 className="key-word">Color</h1>
+                    <h1 className="key-word">estado</h1>
 
                     <p className="main-content">
-                        Color define que color usara el boton siendo 5 colores posibles (primary, success,
-                        info, warning, error) esto son los colores posibles que se le pueden asignar.
+                        Esto define cuando se abre el modal ya que cuando sea true el modal 
+                        se muestra y al cambiar se oculta, se pasa como prop parea que desde 
+                        afuera se habra el modal
                     </p>
 
-                    <h1 className="key-word">Evento</h1>
+                    <h1 className="key-word">close</h1>
 
                     <p className="main-content">
-                        Evento es la funcion que se ejecutara al momento de hacer click en el boton
-                        puediendo manejar de esta manera todo tipo de acciones.
+                        Esta funcion se usa para cerrar, se envia como prop para que se pueda manejar 
+                        algun evento al momento de cerrar el modoal, la funcion se ejecuta al hacer 
+                        click fuera del modal
                     </p>
+
+                    <h1 className="key-word">persistent</h1>
+
+                    <p className="main-content">
+                        Esta propiedad permite que el modal persista al hacer click fuera 
+                        del mismo o que por el contrario se cierre al pulsar fuera del modal, 
+                        se espera que sea un booleano
+                    </p>
+
                 </DivMainContent>
             </Panel>
         </>
