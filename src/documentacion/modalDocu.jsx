e62a9aa5698af2d7a5e1307078e7107e2492e90d
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-10-05 13:04:41 -0600

    feat: Se agrego una pagina para la documentacion del modal

---
+++ src/documentacion/modalDocu.jsx
@@ -0,0 +1,48 @@
+import { DivMainContent, Panel } from "../componentes/contenedores";
+import { Lista } from "../componentes/Lista";
+
+
+export function ModalDocu() {
+
+    const propsModal = ["children", "color", "evento"];
+
+    return (
+        <>
+            <Panel>
+                <DivMainContent>
+                    <h1 className="main-subtitle">Boton</h1>
+
+                    <h1 className="key-word">Props</h1>
+
+                    <p className="main-content">
+                        El componente esta creado con las siguientes props:
+                    </p>
+
+                    <Lista elementos={propsModal} />
+
+                    <h1 className="key-word">children</h1>
+
+                    <p className="main-content">
+                        La propiedad children permite que el componente sea usaso con una etiqueta
+                        de apertura y de cierre siendo el contenido de la etiqueta lo que se muestra
+                        en el boton.
+                    </p>
+
+                    <h1 className="key-word">Color</h1>
+
+                    <p className="main-content">
+                        Color define que color usara el boton siendo 5 colores posibles (primary, success,
+                        info, warning, error) esto son los colores posibles que se le pueden asignar.
+                    </p>
+
+                    <h1 className="key-word">Evento</h1>
+
+                    <p className="main-content">
+                        Evento es la funcion que se ejecutara al momento de hacer click en el boton
+                        puediendo manejar de esta manera todo tipo de acciones.
+                    </p>
+                </DivMainContent>
+            </Panel>
+        </>
+    )
+}
