66d3062bfbe9296ba9bb9ba537b26c5a3ec772f7
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-10-05 10:50:56 -0600

    fix: Se removio el componente independiente para que ahora solo se use la implementacion independiente de cada boton

--- src/componentes/Btn.jsx
+++
@@ -1,8 +0,0 @@
-import './componentes.css';
-
-export function Btn({children,variant,color='principal',evento}){
-
-    return(
-        <button onClick={evento} className={`btn-basic ${variant} ${color}`} >{children}</button>
-    );
-}
