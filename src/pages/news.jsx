754b16fd2d2bf2a8ac882f5ce5f6ebb17505d08c
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-11-10 17:25:02 -0600

    feat: Se actualizo la version de la plantilla

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -21,7 +21,7 @@ export function News() {
         <Panel>
             <DivRow>
                 <DivMainContent>
-                    <h1 className="main-title">Ui Steve 0.19.23 Gemmel (Alfa)</h1>
+                    <h1 className="main-title">Ui Steve 0.19.24 Gemmel (Alfa)</h1>
 
                     <hr className="divider" />
 
