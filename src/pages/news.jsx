bf808eb0208cdcadd5286df2ae7e9fd9cee335d6
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-30 11:45:34 -0600

    docu: Se actualizo la version

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -23,7 +23,7 @@ export function News() {
         <Panel>
             <DivRow>
                 <DivMainContent>
-                    <h1 className="main-title">Ui Steve 0.19.28 Gemmel (Alfa)</h1>
+                    <h1 className="main-title">Ui Steve 0.20.30 Gemmel (Alfa)</h1>
 
                     <hr className="divider" />
 
