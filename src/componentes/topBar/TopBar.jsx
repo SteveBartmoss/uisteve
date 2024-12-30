6081b88ee395fafc2ad615d73d1595d34a3b3944
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-30 11:47:32 -0600

    feat: Se agrego un nuevo componente para la barra superior

---
+++ src/componentes/topBar/TopBar.jsx
@@ -0,0 +1,13 @@
+import './TopBar.css';
+
+export function TopBar(){
+    return(
+        <>
+            <div className="top-bar-background">
+                <span>User image</span>
+                <span>Config menu</span>
+                <span>Top Bar</span>
+            </div>
+        </>
+    )
+}
