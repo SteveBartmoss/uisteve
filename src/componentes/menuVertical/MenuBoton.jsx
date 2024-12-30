82236933c00fd397f87d768c0620a2ee3682c783
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-30 11:46:15 -0600

    feat: Se implemento una propiedad para mostrar el texto del boton

--- src/componentes/menuVertical/MenuBoton.jsx
+++ src/componentes/menuVertical/MenuBoton.jsx
@@ -1,14 +1,14 @@
 import { NavLink } from "react-router-dom";
 import { Icon } from "./Icon";
 
-export function MenuBoton({evento,isOpen}) {
+export function MenuBoton({evento,isOpen, label=""}) {
     return (
         <div className="div-menu-btn" onClick={evento}>
             <div className="element-menu-btn">
                 <Icon icono="menu" color="md-light" />
             </div>
             {
-                isOpen ? <div><NavLink className="menu-item" to='#'>Ui Steve</NavLink></div> : <></>
+                isOpen ? <div><NavLink className="menu-item" to='#'>{label}</NavLink></div> : <></>
             }
         </div>
     )
