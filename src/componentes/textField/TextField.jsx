72f7f684438ca395ea04e3f981b45d4193a22b14
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-01-07 07:48:15 -0600

    Se cambio la estructura del textField para que ahora no se necesite un color de fondo

--- src/componentes/textField/TextField.jsx
+++ src/componentes/textField/TextField.jsx
@@ -1,12 +1,12 @@
 import './TextField.css';
 
-export function TextField({label,isPass=false,text,action}) {
+export function TextField({label,isPass,text,action}) {
 
     return (
         <>
             <div className="form">
-                <input type={isPass ? 'password' : 'text'} id="prueba" value={text} onChange={(event)=>action(event)} className="text-field" placeholder='' />
-                <label htmlFor="field" className="text-field-label">{label}</label>
+                <input type={isPass ? 'password' : 'text'} id="text-field-input" value={text} onChange={(event)=>action(event)} className="text-field" placeholder='' />
+                <label htmlFor="text-field-input" className="text-field-label">{label}</label>
             </div>
         </>
     );
