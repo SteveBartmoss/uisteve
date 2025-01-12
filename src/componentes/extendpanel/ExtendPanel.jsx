530f453dd26b04afd9e7dd7d26c1a99b5ed1cc97
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-01-12 17:15:02 -0600

    feat: Se agrego la propiedad del tamanio minimo que puede tomar un panel extend

--- src/componentes/extendpanel/ExtendPanel.jsx
+++ src/componentes/extendpanel/ExtendPanel.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import './ExtendPanel.css'
 
-export function ExtendPanel({ children, title }) {
+export function ExtendPanel({ children, title, size=0 }) {
 
     const [collapse,setCollapse]=useState(false)
 
@@ -9,7 +10,7 @@ export function ExtendPanel({ children, title }) {
     }
 
     return (
-        <div className="extend-panel">
+        <div style={{minWidth: `${size}rem`}} className="extend-panel">
             <div className="extend-head" onClick={()=>onChangeCollapse()}>
                 <div className="extend-title">{title}</div>
                 <img alt="flecha" className={collapse ? 'down-icon' : 'up-icon'} src="/Assets/expand.svg" />
