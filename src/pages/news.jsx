02ebb2e0baac565ddc7414ed905bea5817e9f14b
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-01-26 21:56:21 -0600

    docu: Se actualizo la version de la libreria

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -22,7 +22,7 @@ export function News() {
         <Panel>
             <DivRow>
                 <DivMainContent>
-                    <h1 className="main-title">Ui Steve 0.21.45 Gemmel (Alfa)</h1>
+                    <h1 className="main-title">Ui Steve 0.22.45 Gemmel (Alfa)</h1>
 
                     <hr className="divider" />
 
